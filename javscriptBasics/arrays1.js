4873444a0c79065bb0304c95266c5188a74b73ca
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-09-08 21:33:26 +0530

    practiced some more methods

--- javscriptBasics/arrays1.js
+++ javscriptBasics/arrays1.js
@@ -15,3 +15,23 @@ function arrayToString(){
     let result = collageNames.toString();
     document.getElementById("toStringResult").innerHTML = result;
 }
+
+function arrayAt(){
+    let result = collageNames.at(2);
+    document.getElementById("arrayAtResult").innerHTML = result;
+}
+
+function arrayJoin(){
+    let result = collageNames.join("*");
+    document.getElementById("arrayJoinResult").innerHTML = result;
+}
+
+function arrayPush(){
+    let result = collageNames.push("KMIT");
+    document.getElementById("arrayPush").innerHTML = result;
+}
+
+function arrayPop(){
+    let result = collageNames.pop();
+    document.getElementById("arrayPop").innerHTML = result;
+}

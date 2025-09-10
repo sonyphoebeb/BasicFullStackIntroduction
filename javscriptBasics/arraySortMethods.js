d0e803132071d801c923ac6e323c56be560c3439
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-09-10 20:02:44 +0530

    more methods practiced

--- javscriptBasics/arraySortMethods.js
+++ javscriptBasics/arraySortMethods.js
@@ -8,7 +8,17 @@ function arraySort(){
     document.getElementById("arraySortResult").innerHTML = myFamily.sort();
 }
 
+//same as sort but toSorted will not alter the original array
+function arrayToSorted(){
+    document.getElementById("arrayToSortedResult").innerHTML = myFamily.toSorted();
+}
+
 //fun to reverse an array
 function arrayReverse(){
     document.getElementById("arrayReverseResult").innerHTML = myFamily.reverse();
+}
+
+//same as sort but toReversed will not alter the original array
+function arrayToReversed(){
+    document.getElementById("arrayToReversedResult").innerHTML = myFamily.toReversed();
 }

3b7f9fbd8e3cc345183dd7141e4743e414334e7d
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-09-10 21:34:14 +0530

    Array sort methods practiced

--- javscriptBasics/arraySortMethods.js
+++ javscriptBasics/arraySortMethods.js
@@ -22,3 +22,17 @@ function arrayReverse(){
 function arrayToReversed(){
     document.getElementById("arrayToReversedResult").innerHTML = myFamily.toReversed();
 }
+
+// Sample Array 2
+let numbers = [20, 50, 10, 305, 100, 119, 90];
+document.getElementById("showArray2").innerHTML = numbers;
+
+//fun to sort num array 1s,2s,3s
+function arraySortNum(){
+    document.getElementById("arraySortNumResult").innerHTML = numbers.sort();
+}
+
+//fun to sort num array correctly by writing a fun
+function arraySortNum2(){
+    document.getElementById("arraySortNum2Result").innerHTML = numbers.sort(function (a,b) {return a - b});
+}

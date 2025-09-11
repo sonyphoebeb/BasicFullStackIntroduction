bb6b8b5ec0b4bf8d3f925eb021ac827a720b0285
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-09-11 19:55:01 +0530

    fisher Yates method practiced

--- javscriptBasics/arraySortMethods.js
+++ javscriptBasics/arraySortMethods.js
@@ -4,35 +4,53 @@ let myFamily = ["Rambabu", "Jyothi", "Shyamala", "Sony", "Nani"];
 document.getElementById("showArray").innerHTML = myFamily;
 
 //fun to sort an array
-function arraySort(){
+function arraySort() {
     document.getElementById("arraySortResult").innerHTML = myFamily.sort();
 }
 
 //same as sort but toSorted will not alter the original array
-function arrayToSorted(){
+function arrayToSorted() {
     document.getElementById("arrayToSortedResult").innerHTML = myFamily.toSorted();
 }
 
 //fun to reverse an array
-function arrayReverse(){
+function arrayReverse() {
     document.getElementById("arrayReverseResult").innerHTML = myFamily.reverse();
 }
 
 //same as sort but toReversed will not alter the original array
-function arrayToReversed(){
+function arrayToReversed() {
     document.getElementById("arrayToReversedResult").innerHTML = myFamily.toReversed();
 }
 
 // Sample Array 2
 let numbers = [20, 50, 10, 305, 100, 119, 90];
-document.getElementById("showArray2").innerHTML = numbers;
+
+window.onload = function () {
+    document.getElementById("showArray2").innerHTML = numbers;
+  };
 
 //fun to sort num array 1s,2s,3s
-function arraySortNum(){
+function arraySortNum() {
     document.getElementById("arraySortNumResult").innerHTML = numbers.sort();
 }
 
 //fun to sort num array correctly by writing a fun
-function arraySortNum2(){
-    document.getElementById("arraySortNum2Result").innerHTML = numbers.sort(function (a,b) {return a - b});
+function arraySortNum2() {
+    document.getElementById("arraySortNum2Result").innerHTML = numbers.sort(function (a, b) { return a - b });
 }
+
+//fun to get random array elements using fisher Yates method
+function fisherYatesShuffle() {
+    debugger;
+    for (let i = numbers.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // Pick a random index from 0 to i
+
+      // Swap numbers[i] and numbers[j]
+      [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+    }
+
+    document.getElementById("fisherYatesShuffleResult").innerHTML = numbers;
+  }
+  
+  

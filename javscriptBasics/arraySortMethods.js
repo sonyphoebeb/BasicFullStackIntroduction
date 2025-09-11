ba8ae80e81c209b175db757b308fbd8676358906
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-09-11 21:07:17 +0530

    object array sort method practiced

--- javscriptBasics/arraySortMethods.js
+++ javscriptBasics/arraySortMethods.js
@@ -28,7 +28,7 @@ let numbers = [20, 50, 10, 305, 100, 119, 90];
 
 window.onload = function () {
     document.getElementById("showArray2").innerHTML = numbers;
-  };
+};
 
 //fun to sort num array 1s,2s,3s
 function arraySortNum() {
@@ -44,13 +44,46 @@ function arraySortNum2() {
 function fisherYatesShuffle() {
     debugger;
     for (let i = numbers.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1)); // Pick a random index from 0 to i
+        let j = Math.floor(Math.random() * (i + 1)); // Pick a random index from 0 to i
 
-      // Swap numbers[i] and numbers[j]
-      [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+        // Swap numbers[i] and numbers[j]
+        [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
     }
 
     document.getElementById("fisherYatesShuffleResult").innerHTML = numbers;
-  }
-  
-  
+}
+
+// fun to find min value by sorting the array to ascending order and getting 0th index value
+function arrayFindMin() {
+    numbers.sort(function (a, b) { return a - b });
+    document.getElementById("arrayFindMinResult").innerHTML = numbers[0];
+}
+
+// fun to find min value using math.min() and spread operator
+function arrayFindMin2() {
+    let result = Math.min(...numbers);
+    document.getElementById("arrayFindMinResult2").innerHTML = result;
+}
+
+// fun to find max value using math.max() and spread operator
+function arrayFindMax() {
+    let result = Math.max(...numbers);
+    document.getElementById("arrayFindMaxResult").innerHTML = result
+}
+
+//  object array sort
+const courses = [
+    { lang: "JavaScript", year: 1995 },
+    { lang: "HTML", year: 1991 },
+    { lang: "Java", year: 1995 }
+];
+
+// Function to sort and display courses
+function displayCourses() {
+    // Sort by year
+    courses.sort(function (a, b) { return a.year - b.year });
+
+    // Display result
+    let result = courses.map(c => `${c.lang} ${c.year}`).join("<br>");
+    document.getElementById("objectArraySort").innerHTML = result;
+}

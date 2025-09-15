df57a5fe595a8566075dae14772459d8b9535a54
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-09-15 23:05:10 +0530

    given bevarages name, ingredients dynamically and displayed them in ul

--- javscriptBasics/class.js
+++ javscriptBasics/class.js
@@ -1,36 +1,45 @@
 class Beverage {
-    constructor(name, ingredients = []) {
-        this.name = name;
-        this.ingredients = ingredients;
+    constructor(name, ingredients) {
+      this.name = name;
+      this.ingredients = ingredients;
     }
-
-    addIngredient(ingredient) {
-        this.ingredients.push(ingredient);
+  }
+  
+  let beverages = []; // store all beverages
+  
+  function addBeverage() {
+    let name = document.getElementById("beverageName").value;
+    let ingredientsText = document.getElementById("beverageIngredients").value;
+  
+    if (name === "" || ingredientsText === "") {
+      alert("Please enter both name and ingredients!");
+      return;
     }
-
-    removeIngredient(ingredient) {
-        this.ingredients = this.ingredients.filter(item => item !== ingredient);
-    }
-
-    listIngredients() {
-        return this.ingredients.join(", ");
-    }
-}
-
-// Global beverage object
-let coffee = new Beverage("Coffee", ["Milk", "Coffee powder", "Sugar"]);
-
-function showIngredients() {
-    document.getElementById("beverageResult").innerHTML =
-        `<b>${coffee.name}</b> Ingredients: ${coffee.listIngredients()}`;
-}
-
-function addCinnamon() {
-    coffee.addIngredient("Cinnamon");
-    showIngredients();
-}
-
-function removeSugar() {
-    coffee.removeIngredient("Sugar");
-    showIngredients();
-}
+  
+    // split ingredients by comma
+    let ingredients = ingredientsText.split(",");
+  
+    // create new beverage
+    let bev = new Beverage(name, ingredients);
+    beverages.push(bev);
+  
+    // clear inputs
+    document.getElementById("beverageName").value = "";
+    document.getElementById("beverageIngredients").value = "";
+  
+    // show updated list
+    showBeverages();
+  }
+  
+  function showBeverages() {
+    let output = "";
+    beverages.forEach(b => {
+      output += `<h3>${b.name}</h3><ul>`;
+      b.ingredients.forEach(ing => {
+        output += `<li>${ing}</li>`;
+      });
+      output += "</ul>";
+    });
+    document.getElementById("beverageList").innerHTML = output;
+  }
+  
